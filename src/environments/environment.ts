ace46a2f3e440c0dcd75f794da6745a0a88c1fdb
Cesar <cesareli17188@gmail.com>
2026-09-09 15:34:57 -0500

    hero, footer y env cambiados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://YOUR_SUPABASE_PROJECT_ID.supabase.co',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+  supabaseUrl: 'https://whlxncobakktxghxdyfw.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndobHhuY29iYWtrdHhnaHhkeWZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0NzAxOTIsImV4cCI6MjEwNDA0NjE5Mn0.kxK1rimna6s4qYXpHICqi3gPB4St7yXe9eoeK_t5sXk',
 };
